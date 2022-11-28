88b1f32aca7352d5512cfbd0a83250dc9418d365
unknown <arosmor917@g.educaand.es>
2022-11-28 10:33:37 +0100

    Comienzo de Proyecto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Tarea } from '../tarea';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  tareaEditando: Tarea;
+
+  constructor(private) {
+    // Crear una tarea vacia
+  }
+
+  clicBotonInsertar() {
+
+
+  }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBi3NRat_XiCiFkjqk0jnt0PB47It9qzdk",
+    authDomain: "proyecto-cloud-firestore-3b89a.firebaseapp.com",
+    projectId: "proyecto-cloud-firestore-3b89a",
+    storageBucket: "proyecto-cloud-firestore-3b89a.appspot.com",
+    messagingSenderId: "483550648189",
+    appId: "1:483550648189:web:cebf2562896ed60c7a1e14",
+    measurementId: "G-C8ENV8EYR8"
+  }
 };
 
 /*
